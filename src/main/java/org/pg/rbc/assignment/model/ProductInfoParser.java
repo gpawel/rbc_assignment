087114a132fd861bac9e6ef11c2377b0f4c2bad0
Pavel <gpawel@gmail.com>
2020-05-17 16:31:23 -0400

    parsing products - xpaths for the element

--- src/main/java/org/pg/rbc/assignment/model/ProductInfoParser.java
+++ src/main/java/org/pg/rbc/assignment/model/ProductInfoParser.java
@@ -12,22 +12,24 @@ public class ProductInfoParser {
     private List<WebElement> products = new ArrayList<WebElement>();;
 
     private By productId = By.xpath("./div[@data-track-product-id]");
-    private By salesText = By.xpath("//div[contains(@class,'product-badge__icon__text product-badge__icon__text--sale')]");
-    private By salesEnds = By.xpath("//div[contains(@class,'product-badge__icon__expiry product-badge__icon__expiry--sale')]");
-    private By productEyeBrow = By.cssSelector(".product-tile__eyebrow");
-    private By productBrand = By.cssSelector(".product-name__item product-name__item--brand");
-    private By productName = By.className(".product-name__item product-name__item--name");
-    private By productSize = By.cssSelector(".product-name__item product-name__item--package-size");
-    private By productText = By.cssSelector(".product-badge__text product-badge__text--product-tile");
-    private By sellingPriceList = By.cssSelector(".selling-price-list selling-price-list--product-tile,product-tile");
-    private By sellingPriceValue = By.cssSelector(".price__value selling-price-list__item__price selling-price-list__item__price--now-price__value");
-    private By sellingPriceWasValue = By.cssSelector(".price selling-price-list__item__price selling-price-list__item__price--was-price");
-    private By sellingPriceType = By.cssSelector(".price__type selling-price-list__item__price selling-price-list__item__price--now-price__type");
-    private By sellingPriceWasType = By.cssSelector(".price__type selling-price-list__item__price selling-price-list__item__price--was-price__type");
-    private By sellingPriceUnit = By.cssSelector(".price__unit selling-price-list__item__price selling-price-list__item__price--now-price__unit");
-    private By comparisonPriceList = By.cssSelector(".comparison-price-list comparison-price-list--product-tile comparison-price-list--product-tile");
-    private By comparisonPriceValue = By.cssSelector(".price__value comparison-price-list__item__price__value");
-    private By comparisonPriceUnit = By.cssSelector(".price__value comparison-price-list__item__price__value");
+    private String salesText = "//div[contains(@class,'product-badge__icon__text product-badge__icon__text--sale')]";
+    private String salesEnds = "//div[contains(@class,'product-badge__icon__expiry product-badge__icon__expiry--sale')]";
+    private String productEyeBrow = "//div[contains(@class,'product-tile__eyebrow')]";
+    private String productBrand = "//span[contains(@class,'product-name__item product-name__item--brand')]";
+    private String productName = "//span[contains(@class,'product-name__item product-name__item--name')]";
+    private String productSize = "//span[contains(@class,'product-name__item product-name__item--package-size')]";
+    private String productText = "//div[contains(@class,'product-badge__text product-badge__text--product-tile')]";
+
+    private String sellingPriceValue = "//span[contains(@class,'price__value selling-price-list__item__price selling-price-list__item__price--now-price__value')]";
+    private String sellingPriceType = "//span[contains(@class,'price__type selling-price-list__item__price selling-price-list__item__price--now-price__type')]/span";
+    private Unit sellingPriceUnit = Unit.EA;
+
+    private String sellingPriceWasValue = "//span[contains(@class,'price__value selling-price-list__item__price selling-price-list__item__price--was-price__value')]";
+    private String sellingPriceWasType = "//span[contains(@class,'price__type selling-price-list__item__price selling-price-list__item__price--was-price__type')]/span";
+
+    private String comparisonPriceValue = "//span[contains(@class,'price__value comparison-price-list__item__price__value')]";
+    private String comparisonPriceUnit = "//span[contains(@class,'price__unit comparison-price-list__item__price__unit')]";
+
 
     private WebDriver driver;
 
@@ -45,10 +47,17 @@ public class ProductInfoParser {
         return parse(products);
     }
 
-    private List<Product> parse(List<WebElement> products) {
+    public List<Product> parse(List<WebElement> products) {
         ArrayList<Product> result = new ArrayList<>();
         for (WebElement productRoot : products) result.add(parseSingleProductElement(productRoot));
         return result;
+    }
+
+    public Product parseProductById (String productId) {
+        String root  = "//div[@data-track-product-id='" + productId + "']/..";
+        String sales = getValue(root+salesText);
+        String salesExpires = getValue(root+salesEnds);
+        return null;
     }
 
     private Product parseSingleProductElement(WebElement root) {
@@ -57,18 +66,15 @@ public class ProductInfoParser {
         return null;
     }
 
-    private Product parseProductById (String prodId) {
-        String root  = "//div[@data-track-product-id='" + productId + "']/..";
-        String sales = getValue(root+salesText);
-        String salesExpires = getValue(root+salesEnds);
-        return null;
-    }
-
     private String getValue(String xpath) {
-        List<WebElement> r = driver.findElements(By.xpath(xpath));
-        if (r.isEmpty()) return "";
-        else return r.get(0).getText();
+        return getValue(xpath,0);
     }
 
+    private String getValue(String xpath, int index) {
+        List<WebElement> r = driver.findElements(By.xpath(xpath));
+        if (index > r.size()-1) throw new RuntimeException(index + " is out of boundaries from results found by " + xpath);
+        if (r.isEmpty()) return "";
+        else return r.get(index).getText();
+    }
 
 }
